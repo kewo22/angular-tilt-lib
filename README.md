7bd087c6049fe8a598ed4444a44c840bdb2a1ad5
kewo22 <kewinf271@gmail.com>
2019-12-24 11:49:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tilt-lib
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tilt-lib)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+  test(e:any):void{
+    // console.log(e.clientX + ' - ' +e.clientY)
+  }
+}
