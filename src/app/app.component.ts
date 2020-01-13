f1f811a2d2d14949acf3e37fd8f4a9b31471ed20
kewo22 <kewinf271@gmail.com>
2020-01-13 19:15:57 +0530

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,53 +5,6 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit {
-  el: HTMLElement;
-
-  elHeight: number = 0;
-  elWidth: number = 0;
-
-  ngOnInit() {
-    this.el = document.querySelector(".one");
-
-    this.elHeight = this.el.clientHeight;
-    this.elWidth = this.el.clientWidth;
-  }
-
-  onMouseMove(e: any): void {
-    // console.log(
-    //   this.test(e.clientY - e.target.offsetTop) +
-    //     " -- " +
-    //     this.test2(e.clientX - e.target.offsetLeft)
-    // );
-
-    let cX: number = this.test(e.clientY - e.target.offsetTop);
-    let cY: number = this.test2(e.clientX - e.target.offsetLeft);
-
-    this.el.style.transform = `perspective(300px) rotateX(${cX}deg) rotateY(${cY}deg)`;
-  }
-
-  test(varY: number): number {
-    const max = this.elWidth / 2;
-    const y = varY; // variable
-
-    const yy = (y / max) * 100;
-    const cy = yy / 10;
-
-    return Math.ceil(cy - 10);
-  }
-
-  test2(varY: number) {
-    const max = this.elHeight / 2;
-    const y = varY; // variable
-
-    const yy = (y / max) * 100;
-    const cy = yy / 10;
-
-    return Math.ceil(10 - cy);
-  }
-
-  onMouseLeave(e: any): void {
-    this.el.style.transform = `perspective(0px) rotateX(0deg) rotateY(0deg)`;
-  }
+export class AppComponent {
+  
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,59 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: "hello",
+  templateUrl: "./hello.component.html",
+  styleUrls: ["./hello.component.css"]
 })
-export class HelloComponent  {
-  @Input() name: string;
+export class HelloComponent implements OnInit {
+  el: HTMLElement;
+
+  elHeight: number = 0;
+  elWidth: number = 0;
+
+  xxx: number = 100;
+
+  ngOnInit() {
+    this.el = document.querySelector(".one");
+
+    this.elHeight = this.el.clientHeight;
+    this.elWidth = this.el.clientWidth;
+  }
+
+  onMouseMove(e: any): void {
+    // console.log(
+    //   this.test(e.clientY - e.target.offsetTop) +
+    //     " -- " +
+    //     this.test2(e.clientX - e.target.offsetLeft)
+    // );
+
+    let cX: number = this.test(e.clientY - e.target.offsetTop);
+    let cY: number = this.test2(e.clientX - e.target.offsetLeft);
+
+    this.el.style.transform = `perspective(${this.xxx}px) rotateX(${cX}deg) rotateY(${cY}deg)`;
+  }
+
+  test(varY: number): number {
+    const max = this.elWidth / 2;
+    const y = varY; // variable
+
+    const yy = (y / max) * 100;
+    const cy = yy / 10;
+
+    return Math.ceil(cy - 10);
+  }
+
+  test2(varY: number) {
+    const max = this.elHeight / 2;
+    const y = varY; // variable
+
+    const yy = (y / max) * 100;
+    const cy = yy / 10;
+
+    return Math.ceil(10 - cy);
+  }
+
+  onMouseLeave(e: any): void {
+    this.el.style.transform = `perspective(0px) rotateX(0deg) rotateY(0deg)`;
+  }
 }
